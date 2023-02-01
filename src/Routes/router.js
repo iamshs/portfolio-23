a44b6b8cd09376cd5134d9f38e7290620a6fa079
Md Sholayman <mdsholayman998@gmail.com>
2023-02-01 21:19:57 +0600

    initial setup done

---
+++ src/Routes/router.js
@@ -0,0 +1,34 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../Layout/Main/Main";
+import About from "../Pages/Main/About";
+import Blog from "../Pages/Main/Blog";
+import Contact from "../Pages/Main/Contact";
+import Home from "../Pages/Main/Home";
+
+const router = createBrowserRouter([
+     {
+        path : "/" ,
+        element : <Main /> ,
+        children : [
+            {
+                path : '/',
+                element : <Home />
+            },
+            {
+                path : 'contact',
+                element : <Contact />
+            },
+            {
+                path : 'about',
+                element : <About />
+            },
+            {
+                path : 'blog',
+                element : <Blog />
+            },
+            
+        ]
+     }
+]);
+
+export default router
