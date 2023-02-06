73286a7e74919d10efcbaa13d0c3891e09e2d23c
Md Sholayman <mdsholayman998@gmail.com>
2023-02-07 01:18:20 +0600

    header completed

---
+++ src/Pages/Main/Home/Background.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Background = () => {
+    return (
+        <section  >
+         
+        </section>
+    );
+};
+
+export default Background;
--- src/Pages/Main/Home/Home.js
+++ src/Pages/Main/Home/Home.js
@@ -1,17 +1,33 @@
-import React from 'react';
-import bg from "../../../assets/bg.jpg"
-
-
+import React from "react";
+import bg from "../../../assets/bg.png";
+import { AiFillGithub } from "react-icons/ai";
 
 const Home = () => {
-    return (
-        <section className='flex items-center justify-center bg-cover min-h-screen' style={{backgroundImage: `url(${bg})` , backgroundColor: '#cccccc'}} >
-         <div >
-         <h1 className='text-5xl font-semibold text-primary '>HELLO , I'M MD SHOLAYMAN</h1>
-          <p className='text-3xl font-semibold'>Welcome to my portfolio.</p>
-         </div>
-        </section>
-    );
+  return (
+    <section
+      className="flex items-center justify-center bg-cover min-h-screen"
+      style={{ backgroundImage: `url(${bg})`, backgroundColor: "#cccccc" }}
+    >
+      <div className="grid place-items-center">
+        <h1 className="lg:text-6xl font-semibold text-secondary ">
+          Hello , I'm MD SHOLAYMAN
+        </h1>
+        <p className="text-2xl font-semibold  mt-8">Welcome to my portfolio.</p>
+        <p className="text-2xl font-semibold text-white">
+          Want to learn more about me? Visit my
+          <button className="btn btn-active btn-link text-success font-bold">
+            <span className="mr-2 text-lg">GitHub</span>
+            <AiFillGithub size={25} />
+          </button>
+        </p>
+        <div className="flex  ">
+        <button className="btn text-lg btn-primary m-1">Download Resume</button>
+        <button className="btn btn-accent text-lg m-1">Contact Me</button>
+        </div>
+      </div>
+      
+    </section>
+  );
 };
 
 export default Home;
