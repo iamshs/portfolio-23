0ae9ef780e63ddaba8fdc42190e85c3b070deca6
Md Sholayman <mdsholayman998@gmail.com>
2023-02-06 21:46:17 +0600

    navbar created

--- src/Routes/router.js
+++ src/Routes/router.js
@@ -1,9 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main/Main";
-import About from "../Pages/Main/About";
-import Blog from "../Pages/Main/Blog";
-import Contact from "../Pages/Main/Contact";
-import Home from "../Pages/Main/Home";
+import About from "../Pages/Main/About/About";
+import Blog from "../Pages/Main/Blog/Blog";
+import Contact from "../Pages/Main/Contact/Contact";
+import Home from "../Pages/Main/Home/Home";
+
 
 const router = createBrowserRouter([
      {
