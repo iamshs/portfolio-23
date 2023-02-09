0bdc6688200715ef101b399dff4a04bab8dcf142
Md Sholayman <mdsholayman998@gmail.com>
2023-02-09 21:51:05 +0600

    all the project link added

--- src/Pages/Main/Home/Home.js
+++ src/Pages/Main/Home/Home.js
@@ -2,36 +2,42 @@ import React from "react";
 import bg from "../../../assets/bg.png";
 import { AiFillGithub } from "react-icons/ai";
 import Projects from "../../../components/Projects";
-import './Home.css'
+import "./Home.css";
 
 const Home = () => {
   return (
-   <>
-    <section
-      className="flex items-center justify-center max-w-10xl bg-cover  bg-no-repeat bg-center h-[50vh] md:h-[60vh] lg:h-[90vh]  "
-      style={{ backgroundImage: `url(${bg})` }}
-    >
-      <div className="grid place-items-center">
-        <h1 className="lg:text-6xl md:text-5xl text-2xl font-semibold text-secondary animate-charcter ">
-          Hello , I'm MD SHOLAYMAN
-        </h1>
-        <p className="lg:text-2xl text-lg font-semibold  mt-8">Welcome to my portfolio.</p>
-        <p className="lg:text-2xl text-lg font-semibold text-white">
-          Want to learn more about me? Visit my
-          <button className="btn btn-active btn-link text-success font-bold">
-            <span className="mr-2 text-lg">GitHub</span>
-            <AiFillGithub size={25} />
-          </button>
-        </p>
-        <div className="flex flex-col lg:flex-row md:flex-row lg:mt-10 md:mt-8 mt-5  ">
-        <button className="btn lg:text-lg btn-primary m-2">Download Resume</button>
-        <button className="btn btn-accent lg:text-lg m-2">Contact Me</button>
+    <>
+      <section
+        className="flex items-center justify-center max-w-10xl bg-cover 
+         bg-no-repeat bg-center h-[50vh] md:h-[60vh] lg:h-[90vh] max-w-10xl"
+        style={{ backgroundImage: `url(${bg})` }}
+      >
+        <div className="grid place-items-center">
+          <h1 className="lg:text-6xl md:text-5xl text-2xl font-semibold text-secondary animate-charcter ">
+            Hello , I'm MD SHOLAYMAN
+          </h1>
+          <p className="lg:text-2xl text-lg font-semibold  mt-8">
+            Welcome to my portfolio.
+          </p>
+          <p className="lg:text-2xl text-lg font-semibold text-white">
+            Want to learn more about me? Visit my
+            <button className="btn btn-active btn-link text-success font-bold">
+              <span className="mr-2 text-lg">GitHub</span>
+              <AiFillGithub size={25} />
+            </button>
+          </p>
+          <div className="flex flex-col lg:flex-row md:flex-row lg:mt-10 md:mt-8 mt-5  ">
+            <button className="btn lg:text-lg btn-primary m-2">
+              Download Resume
+            </button>
+            <button className="btn btn-accent lg:text-lg m-2">
+              Contact Me
+            </button>
+          </div>
         </div>
-      </div>
-      
-    </section>
-    <Projects />
-   </>
+      </section>
+      <Projects />
+    </>
   );
 };
 
--- src/components/Project.js
+++ src/components/Project.js
@@ -1,17 +1,32 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Project = ({ project }) => {
+  const {name,image1 , client , live,server} = project
+
+  const goToLiveSite =() =>{
+    <Link to={window.open(`${live}`)}></Link>
+  }
+  const goToClientGit =() =>{
+    <Link to={window.open(`${client}`)}></Link>
+  }
+  const goToServerGit =() =>{
+    <Link to={window.open(`${server}`)}></Link>
+  }
   return (
     <div className="card rounded max-w-md bg-[#0D0D0D] shadow-xl place-items-center grid ">
       
-        <img src={project.image1} alt="" />
-        <h1 className="lg:text-4xl text-3xl font-bold mt-10 mb-4 ">{project.name} </h1>
+        <img src={image1} alt="" />
+        <h1 className="lg:text-4xl text-3xl font-bold mt-10 mb-4 ">{name} </h1>
         <div className="flex justify-center ">
         
-        <button className="btn btn-primary btn-sm mr-2">Live Site</button>
-        <button className="btn btn-accent btn-sm ml-2">Client Git</button>
+        <button onClick={goToLiveSite} className="btn btn-primary btn-sm mr-2">Live Site</button>
+        <button onClick={goToClientGit} className="btn btn-accent btn-sm ml-2">Client Git</button>
+        {
+          server && <button onClick={goToServerGit} className="btn btn-secondary btn-sm ml-2">Server Git</button>
+        }
         </div> 
-        <button className="btn btn-outline w-[65%] font-bold rounded lg:text-lg  lg:my-8 my-6">Project Details</button>
+        <button  className="btn btn-outline w-[65%] font-bold rounded lg:text-lg  lg:my-8 my-6">Project Details</button>
       
     </div>
   );
