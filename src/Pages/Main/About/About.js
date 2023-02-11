6b29aadfbd7edc750e7ffade2c67c2496b51c46e
Md Sholayman <mdsholayman998@gmail.com>
2023-02-11 07:35:25 +0600

    finished the work for now

--- src/Pages/Main/About/About.js
+++ src/Pages/Main/About/About.js
@@ -1,6 +1,7 @@
 import React from "react";
 import me from "../../../assets/me.jpg";
 import bgPo from '../../../assets/bgPo.jpg'
+import './About.css'
 
 const About = () => {
   return (
@@ -11,10 +12,10 @@ const About = () => {
         backgroundRepeat: "no-repeat",
         backgroundPosition:"center"
        
-      }} className="flex flex-col-reverse lg:flex-row items-center justify-center lg:p-20 p-8">
+      }} className="flex flex-col-reverse lg:flex-row items-center justify-center  lg:py-24 p-8">
       <div className="lg:max-w-[50%] lg:p-14 p-2 ">
         <h1 className="title lg:text-5xl md:text-4xl text-3xl text-warning my-5">About me</h1>
-        <p className="lg:text-3xl md:text-2xl text-xl ">
+        <p className="lg:text-2xl md:text-2xl text-xl ">
           My Name is Md Sholayman.I am a full-stack developer.I have experience
           in front-end web development, working with technologies like
           React,Tailwind CSS, React Bootstrap , express.js , firebase,
@@ -22,8 +23,11 @@ const About = () => {
           Programming World.
         </p>
       </div>
-      <div className="avatar">
+      <div className="avatar container">
   <div className="lg:w-[35rem] w-[20rem]  mb-10 lg:mb-0  rounded-full">
+    <img src={me} alt='' />
+    <img src={me} alt='' />
+    <img src={me} alt='' />
     <img src={me} alt='' />
   </div>
 </div>
